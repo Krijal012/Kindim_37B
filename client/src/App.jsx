89082254860025363a36f19a8b83be929adf228a
krijal7@gmail.com <krijalshrestha7@gmail.com>
2026-01-20 18:48:47 +0545

    checking logout function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,30 @@ import PublicRoutes from "./routes/PublicRoutes";
 import PrivateRoutes from "./routes/PrivateRoutes";
 
 function App() {
-  // Check if user is logged in by checking localStorage token
-  const [token, setToken] = useState(localStorage.getItem("access_token"));
+   // Check if user is logged in by checking localStorage token
+   const [token, setToken] = useState(localStorage.getItem("access_token"));
 
-  // Function to update token state after login
-  const handleLogin = () => {
-    setToken(localStorage.getItem("access_token"));
-  };
+   // Function to update token state after login
+   const handleLogin = () => {
+      setToken(localStorage.getItem("access_token"));
+   };
 
-  return (
-    <>
-      {/* If token exists, show private routes. Otherwise show public routes */}
-      {token ? (
-        <PrivateRoutes />
-      ) : (
-        <PublicRoutes onLogin={handleLogin} />
-      )}
-    </>
-  );
+   // Function to handle logout
+   const handleLogout = () => {
+      localStorage.removeItem("access_token");
+      setToken(null);
+   };
+
+   return (
+      <>
+         {/* If token exists, show private routes. Otherwise show public routes */}
+         {token ? (
+            <PrivateRoutes onLogout={handleLogout} />
+         ) : (
+            <PublicRoutes onLogin={handleLogin} />
+         )}
+      </>
+   );
 }
 
 export default App;
