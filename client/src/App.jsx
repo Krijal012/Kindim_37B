2ba2bc3f49d9ac55552ff65b127bc91c83aaa27e
krijal7@gmail.com <krijalshrestha7@gmail.com>
2026-01-21 23:06:56 +0545

    checking cartpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CategorySection from './pages/private/CategorySection';
-import { ErrorPage } from './pages/public/ErrorPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CartPage from "./pages/private/CartPage";
+import CategorySection from "./pages/private/CategorySection";
+import { ErrorPage } from "./pages/public/ErrorPage"; // Add this import
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CategorySection />} />
-        <Route path="/products" element={<CategorySection />} />
+        <Route path="/cartpage" element={<CartPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
