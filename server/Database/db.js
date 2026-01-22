5c305625ff92d8f35f1ec70cfc46405191f26d6c
krijal7@gmail.com <krijalshrestha7@gmail.com>
2026-01-22 12:35:04 +0545

    changed postgres name and password for running server

--- server/Database/db.js
+++ server/Database/db.js
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize(
     "Kindim",
     "postgres",
-    "iamsubi10@",
+    "admin123",
     {
         "host": "localhost",
         "dialect": "postgres",
