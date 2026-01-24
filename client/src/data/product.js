ea5014e515af2463d8448eca4797e7f3882ee35d
Pratyush Adhikari <pratyushadhikari785@gmail.com>
2026-01-24 22:33:25 +0545

    added the dummy dynamic data

---
+++ client/src/data/product.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: 1,
+    name: "Product Name",
+    price: "$120",
+    rating: 3,
+    image: "https://via.placeholder.com/200"
+  },
+  {
+    id: 2,
+    name: "Retro TV",
+    price: "$220",
+    rating: 4,
+    image: "https://upload.wikimedia.org/wikipedia/commons/3/3a/CRT_Television.jpg"
+  },
+  {
+    id: 3,
+    name: "Headphones",
+    price: "$99",
+    rating: 2,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8f/Headphones_1.jpg"
+  },
+  {
+    id: 4,
+    name: "Headphones",
+    price: "$89",
+    rating: 3,
+    image: "https://upload.wikimedia.org/wikipedia/commons/8/8f/Headphones_1.jpg"
+  },
+];
