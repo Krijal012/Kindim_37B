64d9abbf3169d0de4699e94aa183f286ec0a10fa
prajinpokhrel <prajinpokhrel@gmail.com>
2026-01-26 16:29:29 +0545

    Backend added in sellerdashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,15 @@ import { useState, useEffect } from "react";
 // import PublicRoutes from "./routes/PublicRoutes";
 // import PrivateRoutes from "./routes/PrivateRoutes";
 // import { AdminDashboard } from "./pages/private/Admindashboard";
-// import { SellerDashboard } from "./pages/private/Sellerdashboard";
-import {MyProducts} from "./pages/private/MyProducts";
+import { SellerDashboard } from "./pages/private/Sellerdashboard";
+// import {MyProducts} from "./pages/private/MyProducts";
 // import {SellerDashboard} from "./pages/private/Sellerdashboard";
 //
 // import { RewardDashboard } from "./pages/private/RewardDashboard";
 
 
 function App() {
-   return <MyProducts/>;
+   return <SellerDashboard/>;
 };
 
 export default App;
