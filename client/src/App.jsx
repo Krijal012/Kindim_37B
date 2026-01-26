20f0fb6bd959c797541087657be84172786efd41
krijal7@gmail.com <krijalshrestha7@gmail.com>
2026-01-26 16:49:09 +0545

    checking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CartPage from "./pages/private/CartPage";
-import CategorySection from "./pages/private/CategorySection";
-import OrderHistoryPage from "./pages/private/OrderHistoryPage";
-import { ErrorPage } from "./pages/public/ErrorPage";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CategorySection />} />
-        <Route path="/cartpage" element={<CartPage />} />
-        <Route path="/orderhistory" element={<OrderHistoryPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
